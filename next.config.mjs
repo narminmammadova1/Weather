57b021f48d5f2a20ceec29e9eb6eae241d1cc85a
narminmammadova1 <nerminytb89@gmail.com>
2024-11-05 15:02:43 +0400

    test4

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['example.com'], // CDN veya dış kaynaklar için.
+  },
+  webpack(config) {
+    // Tailwind CSS'i özelleştirmek için gerekli değişiklikler eklenebilir
+    return config;
+  },
 };
 
 export default nextConfig;
